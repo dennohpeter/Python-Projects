eeb40e65bdf437f472c1242941a05941c4776c2b
de-ma <dabuadas@mail.uoguelph.ca>
2017-05-15 14:52:51 -0400

    Finished cis 1500 a3 - minus looping

--- CIS1500Assign3.py
+++ CIS1500Assign3.py
@@ -15,27 +15,38 @@
 ##replaceInString does not do the replacing of text directly.  It calls findSubString and insertString.
 ##The function returns 0 if the operation fails and 1 if the operation is successful.
 def replaceInString(original, substring, replace):
+  location = findSubString(original, substring)
+  print(location) #remove later
+  if location == -1:
+    print("Substring not found!")
     return 0
+  else: 
+    newString = insertString(original, location, len(substring), replace)
+    #print("NEW STRING:", newString)
+    return newString
 
 ##This function finds the first occurrence of toFind in original
 ##and returns an integer representing the index of the first element of the toFind substring in original.
 ##For example if toFind was 'cat'
 ##and original was 'thundercat' the function would return 7 since the begining of 'cat' in 'thundercat' is position 7.
 ##If the function does not find a substring it should return -1
-##note:  do not use existing substring functions such as strstr as a solution for this function.
-##Solve the algorithm yourself. You may use basic string functions such as strlen and strcat.
+
 def findSubString(original, toFind):
-    return 0
+  location = original.find(toFind)
+  return location
 
 ##This function replaces the contents of original  with the following sequence of characters:  
 ##1.the characters from original up to position start-1
 ##2.the characters from toInsert 
 ##3.the remaining characters of original, excluding the characters of the substring that is being removed.
 def insertString(original, start, length, toInsert):
-    return 0
+  before = original[:start]
+  after = original[len(before) + length:]
+  final = before + toInsert + after
+  #print("FINAL:", final)
+  return final
     
-
-    
+      
 buffer1 = 'dema is the best'
 buffer2 = 'what is life'
 buffer3 = 'lets see how hard this will be'
@@ -56,42 +67,113 @@ if userChange == 1:
   if userAction == 1:
     print("Enter the new sentence:")
     buffer1 = input()
-
+    print("NEW SENTENCES\n")
     print("1.",buffer1)
     print("2.",buffer2)
     print("3.",buffer3)
     print("4.",buffer4)
     print("5.",buffer5)
-  else:
-    print("ugly")
+  elif userAction == 2:
+    substring = input("Enter the substring you want to replace:")
+    replace = input("Enter the substring you would like to replace it with:")
+    buffer1 = replaceInString(buffer1, substring, replace)
+    print("\nNEW SENTENCES")
+    print("1.",buffer1)
+    print("2.",buffer2)
+    print("3.",buffer3)
+    print("4.",buffer4)
+    print("5.",buffer5)
     
 elif userChange == 2:
-  print("Enter 1 to change the entire sentence\nEnter 2 to change a string\n")
+  print("Enter 1 to change the entire sentence\nEnter 2 to change a string:")
   userAction = int(input())
   if userAction == 1:
     print("Enter the new sentence:\n")
     buffer2 = input()
-
+    print("NEW SENTENCES\n")
+    print("1.",buffer1)
+    print("2.",buffer2)
+    print("3.",buffer3)
+    print("4.",buffer4)
+    print("5.",buffer5)
+  elif userAction == 2:
+    substring = input("Enter the substring you want to replace:")
+    replace = input("Enter the substring you would like to replace it with:")
+    buffer2 = replaceInString(buffer2, substring, replace)
+    print("\nNEW SENTENCES")
+    print("1.",buffer1)
+    print("2.",buffer2)
+    print("3.",buffer3)
+    print("4.",buffer4)
+    print("5.",buffer5)
     
 elif userChange == 3:
-  print("Enter 1 to change the entire sentence\nEnter 2 to change a string\n")
+  print("Enter 1 to change the entire sentence\nEnter 2 to change a string:")
   userAction = int(input())
   if userAction == 1:
     print("Enter the new sentence:\n")
     buffer3 = input()
+    print("NEW SENTENCES")
+    print("1.",buffer1)
+    print("2.",buffer2)
+    print("3.",buffer3)
+    print("4.",buffer4)
+    print("5.",buffer5)
+  elif userAction == 2:
+    substring = input("Enter the substring you want to replace:")
+    replace = input("Enter the substring you would like to replace it with:")
+    buffer3 = replaceInString(buffer3, substring, replace)
+    print("\nNEW SENTENCES")
+    print("1.",buffer1)
+    print("2.",buffer2)
+    print("3.",buffer3)
+    print("4.",buffer4)
+    print("5.",buffer5)
 
     
 elif userChange == 4:
-  print("Enter 1 to change the entire sentence\nEnter 2 to change a string\n")
+  print("Enter 1 to change the entire sentence\nEnter 2 to change a string:")
   userAction = int(input())
   if userAction == 1:
     print("Enter the new sentence:\n")
     buffer4 = input()
+    print("NEW SENTENCES\n")
+    print("1.",buffer1)
+    print("2.",buffer2)
+    print("3.",buffer3)
+    print("4.",buffer4)
+    print("5.",buffer5)
+  elif userAction == 2:
+    substring = input("Enter the substring you want to replace:")
+    replace = input("Enter the substring you would like to replace it with:")
+    buffer4 = replaceInString(buffer4, substring, replace)
+    print("\nNEW SENTENCES")
+    print("1.",buffer1)
+    print("2.",buffer2)
+    print("3.",buffer3)
+    print("4.",buffer4)
+    print("5.",buffer5)
 
     
 elif userChange == 5:
-  print("Enter 1 to change the entire sentence\nEnter 2 to change a string\n")
+  print("Enter 1 to change the entire sentence\nEnter 2 to change a string:")
   userAction = int(input())
   if userAction == 1:
     print("Enter the new sentence:\n")
     buffer5 = input()
+    print("NEW SENTENCES\n")
+    print("1.",buffer1)
+    print("2.",buffer2)
+    print("3.",buffer3)
+    print("4.",buffer4)
+    print("5.",buffer5)
+  elif userAction == 2:
+    substring = input("Enter the substring you want to replace:")
+    replace = input("Enter the substring you would like to replace it with:")
+    buffer5 = replaceInString(buffer5, substring, replace)
+    print("\nNEW SENTENCES")
+    print("1.",buffer1)
+    print("2.",buffer2)
+    print("3.",buffer3)
+    print("4.",buffer4)
+    print("5.",buffer5)
